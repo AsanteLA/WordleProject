20d95837ce07cc92f3a1830409f732f8f4101137
caitlynstoked <119529327+caitlynstoked@users.noreply.github.com>
2024-01-22 19:25:59 -0700

    Update randomtest.py
    fixed the find in list

--- randomtest.py
+++ randomtest.py
@@ -1,22 +1,11 @@
 import random
 from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, N_COLS, N_ROWS
 
 #Selectong the random word
 random_word = random.choice(FIVE_LETTER_WORDS)
 
 #Breaking down a word into a list of characters
-charlist = list(random_word)
-
-#Revelaing the cosen random word
-print('Random Word:', random_word)
-
-#Printing chatracterr list in array format
-print(charlist)
-
-
-# Sekected_Word = random.choice(FIVE_LETTER_WORDS)
-
-# Breaking down a word into a list of characters
 charlist = list(random_word)
 
 # Creating a Wordle window
@@ -37,9 +26,27 @@ for col, char in enumerate(charlist):
 input("Press Enter to exit")
 
 # Accessing and constructing the first guess
+# Accessing and constructing the first guess
 guess = ''
 for row in range(N_ROWS):
     for col in range(N_COLS):
         guess += wordle_window.get_square_letter(row, col) 
 
-# Sekected_Word = random.choice(FIVE_LETTER_WORDS)
+guess_lower = guess.lower()
+
+guess_lower = guess_lower.strip()
+
+def enter_action(FIVE_LETTER_WORDS, guess_lower):
+    
+    if guess_lower in FIVE_LETTER_WORDS:
+        msg = "Good try! This is a word!"
+        wordle_window.show_message(msg, color="Black")
+    else:
+        msg = "Not in word list"
+        wordle_window.show_message(msg, color="Black")
+
+# Call the function with the actual list
+enter_action(FIVE_LETTER_WORDS, guess_lower)
+
+#gw = WordleGWindow()
+#gw.add_enter_listener(enter_action)
